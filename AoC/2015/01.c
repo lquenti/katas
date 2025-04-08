132299a3535b972f3c3e79d4d38d2273be183771
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2025-04-08 13:44:13 +0200

    2015 01 02

--- AoC/2015/01.c
+++ AoC/2015/01.c
@@ -1,12 +1,18 @@
+#include <limits.h>
 #include <stdio.h>
 int main() {
-  int i=0;
-  for (char c; (c = getchar()) != EOF;) {
+  int i=0; int minus_one_idx=1;
+  for (char c; (c = getchar()) != EOF; minus_one_idx++) {
     if (c == '(') {
       i++;
     } else if (c == ')') {
       i--;
     } else {/* who cares */}
+    if (i == -1 && minus_one_idx > 0) {
+      printf("%d\n", minus_one_idx);
+      // i only want to know it first time, dont care anymore
+      minus_one_idx = INT_MIN;
+    }
   }
   printf("%d\n", i);
 }
