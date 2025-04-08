998aa6ddcb778cdee37f4454e6b2e1c2fcdb1c17
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2025-04-08 14:01:05 +0200

    2015 02 01

---
+++ AoC/2015/02.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+int min(int a, int b) {
+  return a<b ? a : b;
+}
+int main() {
+  char *line = NULL;
+  size_t len = 0;
+  int l,w,h;
+  long sum=0;
+  while (getline(&line, &len, stdin) != EOF) {
+    assert(sscanf(line, "%dx%dx%d", &l, &w, &h) == 3);
+    sum += 2*l*w + 2*w*h + 2*h*l;
+    sum += min(l*w, min(w*h, h*l));
+  }
+  printf("sum: %ld\n", sum);
+  free(line);
+}
