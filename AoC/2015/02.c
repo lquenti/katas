0cf40bab736742784bbf27117b0e01e0f0d2c2a7
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2025-04-08 14:07:58 +0200

    2015 02 02

--- AoC/2015/02.c
+++ AoC/2015/02.c
@@ -8,12 +8,16 @@ int main() {
   char *line = NULL;
   size_t len = 0;
   int l,w,h;
-  long sum=0;
+  long sum=0, sum2=0;
   while (getline(&line, &len, stdin) != EOF) {
     assert(sscanf(line, "%dx%dx%d", &l, &w, &h) == 3);
     sum += 2*l*w + 2*w*h + 2*h*l;
     sum += min(l*w, min(w*h, h*l));
+
+    sum2 += min(l+l+w+w, l+l+h+h);
+    sum2 += l*w*h;
   }
   printf("sum: %ld\n", sum);
+  printf("sum2: %ld\n", sum2);
   free(line);
 }
