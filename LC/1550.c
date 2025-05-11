729850b674672c89bad94516a180beed0bbf9714
Lars Quentin <lars@lquenti.de>
2025-05-11 22:34:04 +0200

    lc 1550

---
+++ LC/1550.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+bool threeConsecutiveOdds(int* arr, int arrSize) {
+  for (int i=0; i<arrSize-2; ++i) {
+    if ((arr[i] % 2 == 1) && (arr[i+1] % 2 == 1) && (arr[i+2] % 2 == 1))
+      return true;
+  }
+  return false;
+}
