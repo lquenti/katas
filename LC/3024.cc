263e25fe4d3a377bf55843d5ec6fb61f60dd2aec
Lars Quentin <lars@lquenti.de>
2025-05-19 11:06:56 +0200

    3024

---
+++ LC/3024.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+      if (nums[0] == nums[1] && nums[1] == nums[2]) {
+        return "equilateral";
+      }
+      sort(nums.begin(), nums.end());
+      if (nums[0]+nums[1] <= nums[2] || nums[1]+nums[2]<=nums[0] || nums[2]+nums[0] <= nums[1]) {
+        return "none";
+      }
+      if (nums[0] == nums[1] || nums[1] == nums[2]) {
+        return "isosceles";
+      }
+      return "scalene";
+    }
+};
