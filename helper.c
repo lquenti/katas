c889243e0bc582d78299e818b12ecd7656c9342f
Lars Quentin <lars@lquenti.de>
2025-06-12 12:57:37 +0200

    c vec impl

--- helper.c
+++ helper.c
@@ -1,3 +1,19 @@
+#include<stdlib.h>
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
+
+struct {
+  int *data;
+  size_t cnt;
+  size_t cap;
+} vec_int;
+
+#define vec_push(xs, x) \
+  do { \
+    if (xs.cnt >= xs.cap) { \
+      xs.cap = (xs.cap == 0) ? 16 : xs.cap*2; \
+      xs.data = realloc(xs.data, xs.cap * sizeof(*xs.data)); \
+    } \
+    xs.data[xs.cnt++] = x; \
+  while (0)
