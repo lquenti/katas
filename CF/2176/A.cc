47f5bfd011463508aaa08270f03d61663eebaf35
Lars Quentin <lars@lquenti.de>
2025-12-29 18:27:52 +0100

    2176A linear

--- CF/2176/A.cc
+++ CF/2176/A.cc
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#if 0
+/* O(n^2) solution: Index on right side; 2 pointer backwards each time until higher elem found */
 int main() {
   int t;
   std::cin >> t;
@@ -21,6 +23,33 @@ int main() {
         }
       }
       r--;
+    }
+    std::cout << cnt << std::endl;
+  }
+}
+#endif
+
+// Linear: go left to right; remember best one
+int main() {
+  int t;
+  std::cin >> t;
+  while (t--) {
+    int n, tmp;
+    std::cin >> n;
+    std::vector<int> xs;
+    while (n--) {
+      std::cin >> tmp;
+      xs.push_back(tmp);
+    }
+
+    size_t cnt{0};
+    int M{xs[0]};
+    for (size_t i{1}; i<xs.size(); ++i) {
+      if (M > xs[i]) { // can be removed, is not even better
+        cnt++;
+        continue;
+      }
+      M = std::max(M, xs[i]);
     }
     std::cout << cnt << std::endl;
   }
