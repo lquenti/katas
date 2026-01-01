36696439db476b685ec84990216764bf12ddd450
Lars Quentin <lars@lquenti.de>
2026-01-01 18:13:01 +0100

    remove-duplicates-from-sorted-array.cc linear

--- NC/remove-duplicates-from-sorted-array.cc
+++ NC/remove-duplicates-from-sorted-array.cc
@@ -2,6 +2,8 @@
 using namespace std;
 class Solution {
   public:
+#if 0
+    /* Trivial N^2 solution */
     int removeDuplicates(vector<int>& nums) {
       for (size_t i{0}; i<nums.size()-1;) {
         if (nums[i] == nums[i+1]) {
@@ -14,5 +16,21 @@ class Solution {
         }
       }
       return nums.size();
+    }
+
+    /* can also be solved in linear time using linear space with a (sorted) std::set */
+#endif
+
+    /* two pointer solution */
+    int removeDuplicates(vector<int>& nums) {
+      size_t l{0}, r{0};
+      while (r<nums.size()) {
+        nums[l] = nums[r];
+        while (r < nums.size() && nums[r] == nums[l]) {
+          r++;
+        }
+        l++;
+      }
+      return static_cast<int>(l);
     }
 };
