857dda278e1910e4cc7eca6a67bb82e910d7c282
Lars Quentin <lars@lquenti.de>
2026-04-03 23:08:37 +0200

    remove element was broken ups

--- NC/remove-element.cc
+++ NC/remove-element.cc
@@ -1,18 +1,17 @@
 #include<bits/stdc++.h>
-using namespace std;
 class Solution {
   public:
-    int removeElement(vector<int>& nums, int val) {
-      size_t i{0}, offset{0};
-      while (i+offset < nums.size()) {
-        nums[i] = nums[i+offset];
-        if (nums[i] == val) {
-          offset++;
-        } else {
-          i++;
+    int removeElement(std::vector<int> &nums, int val) {
+      size_t l{0}, r{0};
+      while (r<nums.size()) {
+        if (nums[r] != val) {
+          if (r!=l) {
+            nums[l]=nums[r];
+          }
+          l++;
         }
+        r++;
       }
-      nums.resize(i);
-      return static_cast<int>(i)+1;
+      return l;
     }
 };
