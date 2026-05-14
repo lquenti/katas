146d0a41c4bc121e792dcd59b40ea3263069bb99
Lars Quentin <lquenti@users.noreply.github.com>
2026-05-14 10:28:37 +0200

    Create 2784.cc

---
+++ LC/2784.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for (int i=0; i<nums.size()-1; ++i) {
+            if (nums[i] != i+1) {
+                return false;
+            }
+        }
+        return nums[nums.size()-1] == nums.size()-1;
+    }
+};
