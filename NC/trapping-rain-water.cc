33967a94bc5e9036ecedc556b7153cc649ebf681
Lars Quentin <lquenti@users.noreply.github.com>
2026-05-30 00:32:51 +0200

    Update trapping-rain-water.cc

--- NC/trapping-rain-water.cc
+++ NC/trapping-rain-water.cc
@@ -20,3 +20,33 @@ public:
     }
 };
 */
+// Prefix Sums O(n) time O(n) space (Suffices for me for now)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res{};
+        vector<int> biggest_left;
+        vector<int> biggest_right;
+        {
+            int curr_left{};
+            for (int i=0; i<height.size(); ++i) {
+                curr_left = max(curr_left, height[i]);
+                biggest_left.push_back(curr_left);
+            }
+        }
+        {
+            int curr_right{};
+            for (int j=height.size()-1; j>=0; --j) {
+                curr_right = max(curr_right, height[j]);
+                biggest_right.push_back(curr_right);
+            }
+        }
+        reverse(biggest_right.begin(), biggest_right.end());
+        for (int i=1; i<height.size()-1; ++i) {
+            if (biggest_left[i] > height[i] && biggest_right[i] > height[i]) {
+                res+=min(biggest_left[i], biggest_right[i])-height[i];
+            }
+        }
+        return res;
+    }
+};
